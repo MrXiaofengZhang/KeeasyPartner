4d7649662f7d900a5dfd97737a60275effa43429
zhangxf <zhangxf@a31a5ab4-c698-49e0-99af-500dd8312fa3>
2016-02-26 09:18:59 +0000

    1

--- ViewController/Self/WPCMyOwnVC.m
+++ ViewController/Self/WPCMyOwnVC.m
@@ -159,12 +159,15 @@
             }else{
                 comentStatus.hidden = NO;
             }
-            if([[LVTools mToString:result[@"data"][@"messageCount"]] isEqualToString:@"0"]&&[[LVTools mToString:result[@"data"][@"fansStatus"]] isEqualToString:@"0"]&&[[LVTools mToString:result[@"data"][@"matchCount"]] isEqualToString:@"0"]&&[[EaseMob sharedInstance].chatManager totalUnreadMessagesCount]==0&&[[LVTools mToString:result[@"data"][@"replyCount"]] isEqualToString:@"0"]){
+            if(([[LVTools mToString:result[@"data"][@"messageCount"]] isEqualToString:@"0"]||[[LVTools mToString:result[@"data"][@"messageCount"]] length]==0)&&
+               ([[LVTools mToString:result[@"data"][@"fansStatus"]] isEqualToString:@"0"]||[[LVTools mToString:result[@"data"][@"fansStatus"]] length]==0)&&
+               ([[LVTools mToString:result[@"data"][@"matchCount"]] isEqualToString:@"0"]||[[LVTools mToString:result[@"data"][@"matchCount"]] length]==0)&&
+               [[EaseMob sharedInstance].chatManager totalUnreadMessagesCount]==0&&
+               ([[LVTools mToString:result[@"data"][@"replyCount"]] isEqualToString:@"0"]||[[LVTools mToString:result[@"data"][@"replyCount"]] length]==0)){
               ((LVMainViewController*)(self.tabBarController)).myCount.hidden = YES;
             }else{
             ((LVMainViewController*)(self.tabBarController)).myCount.hidden = NO;
             }
-
         }
         else{
             [self showHint:ErrorWord];
