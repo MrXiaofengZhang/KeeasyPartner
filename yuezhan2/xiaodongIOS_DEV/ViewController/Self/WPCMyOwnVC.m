a5b02afd27aaee10659538daa0f22269efdafef7
zhangxf <zhangxf@a31a5ab4-c698-49e0-99af-500dd8312fa3>
2016-02-29 07:31:33 +0000

    1

--- yuezhan2/xiaodongIOS_DEV/ViewController/Self/WPCMyOwnVC.m
+++ yuezhan2/xiaodongIOS_DEV/ViewController/Self/WPCMyOwnVC.m
@@ -879,6 +879,7 @@
 }
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:YES];
+    self.navigationController.navigationBar.hidden = NO;
 }
 
 - (void)didReceiveMemoryWarning {
