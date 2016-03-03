5d5d5408b59200defe5648a85ed864bc8860393a
zhangxf <zhangxf@a31a5ab4-c698-49e0-99af-500dd8312fa3>
2016-03-03 09:52:55 +0000

    1

---
+++ yuezhan2/xiaodongIOS_DEV/ViewController/Self/AddressModel/ChineseString.h
@@ -0,0 +1,21 @@
+//
+//  ChineseString.h
+//  ZiMuPaiXuDemo
+//
+//  Created by Co_Lee on 14-7-11.
+//  Copyright (c) 2014年 ___ShenKeYi___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChineseString : NSObject
+
+@property(nonatomic,copy)NSString *string;
+
+@property(nonatomic,copy)NSString *pinYin;
+
+@property(nonatomic,copy)NSString *nickName;
+
+@property(nonatomic,copy)NSString *tel;
+
+@end
---
+++ yuezhan2/xiaodongIOS_DEV/ViewController/Self/AddressModel/pinyin.m
@@ -0,0 +1,13 @@
+//
+//  pinyin.m
+//  ZiMuPaiXuDemo
+//
+//  Created by Co_Lee on 14-7-11.
+//  Copyright (c) 2014年 ___ShenKeYi___. All rights reserved.
+//
+
+#import "pinyin.h"
+
+@implementation pinyin
+
+@end
