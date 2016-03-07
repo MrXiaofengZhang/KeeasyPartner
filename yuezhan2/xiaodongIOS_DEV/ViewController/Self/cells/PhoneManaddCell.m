b88d0af42ab98c4e7c9901f5b080b7c0d8365037
zhangxf <zhangxf@a31a5ab4-c698-49e0-99af-500dd8312fa3>
2016-03-07 06:23:56 +0000

    1

--- yuezhan2/xiaodongIOS_DEV/ViewController/Self/cells/PhoneManaddCell.m
+++ yuezhan2/xiaodongIOS_DEV/ViewController/Self/cells/PhoneManaddCell.m
@@ -24,9 +24,18 @@
     if (_addBtn == nil) {
         _addBtn = [[UIButton alloc] initWithFrame:CGRectMake(BOUNDS.size.width-80, 15.0, 50.0, 25.0)];
         _addBtn.layer.cornerRadius = mygap;
-        [_addBtn setBackgroundColor:SystemBlue];
+        _addBtn.layer.masksToBounds = YES;
+        [_addBtn setBackgroundImage:[LVTools buttonImageFromColor:SystemBlue withFrame:_addBtn.frame] forState:UIControlStateNormal];
         [_addBtn setTitle:@"添加" forState:UIControlStateNormal];
         [_addBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        
+        [_addBtn setBackgroundImage:[LVTools buttonImageFromColor:[UIColor clearColor] withFrame:_addBtn.frame] forState:UIControlStateDisabled];
+        [_addBtn setTitle:@"已添加" forState:UIControlStateDisabled];
+        [_addBtn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
+        
+        [_addBtn setBackgroundImage:[LVTools buttonImageFromColor:[UIColor clearColor] withFrame:_addBtn.frame] forState:UIControlStateSelected];
+        [_addBtn setTitle:@"已发送" forState:UIControlStateSelected];
+        [_addBtn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateSelected];
         _addBtn.titleLabel.font = [UIFont systemFontOfSize:14.0];
         
     }
