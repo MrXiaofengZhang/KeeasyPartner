29bd64f8cbb6c7d835371aa0514e14ecb5064096
zhangxf <zhangxf@a31a5ab4-c698-49e0-99af-500dd8312fa3>
2016-03-07 06:24:41 +0000

    1

--- ViewController/Self/PhoneListController.m
+++ ViewController/Self/PhoneListController.m
@@ -19,13 +19,16 @@
 
 #import "PhoneListController.h"
 #import <AddressBook/AddressBook.h>
-#import <AddressBookUI/AddressBookUI.h>
+//#import <AddressBookUI/AddressBookUI.h>
 #import "pinyin.h"
 #import "ChineseString.h"
 #import "CoShuXing.h"
 #import "SearchCoreManager.h"
 #import "PhoneManaddCell.h"
-@interface PhoneListController ()<UITableViewDataSource,UISearchBarDelegate,UISearchDisplayDelegate>
+#import "FriendListModel.h"
+#import "WPCFriednMsgVC.h"
+#import "LVSportViewController.h"
+@interface PhoneListController ()<UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate,UISearchDisplayDelegate>
 {
     NSMutableArray *_resultArray;
     
@@ -37,6 +40,9 @@
     
     UISearchBar *_searchBar;
 
+    NSMutableArray *_phoneArray;
+    
+    NSMutableArray *_infoArray;
 }
 @property (nonatomic, strong)NSMutableArray *dataArr;
 
@@ -67,9 +73,12 @@
     // Do any additional setup after loading the view.
     self.title = @"手机联系人";
     [self navgationBarLeftReturn];
-    
+    if (self.hasRightBtn) {
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStyleBordered target:self action:@selector(okOnClick)];
+    }
     _addressBookTemp = [NSMutableArray array];
     
+    _phoneArray = [NSMutableArray array];
     
     
     self.searchByName = [[NSMutableArray alloc]init];
@@ -87,25 +96,47 @@
     [self createTableView];
 
 }
+- (void)okOnClick{
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
 #pragma mark   create method
 - (void)getAddressList{
-    
+    //iOS6之前
+//    addBook =ABAddressBookCreate();
+    //iOS6之后
+    //app是否获得通讯录授权
+    int __block tip = 0;
     ABAddressBookRef addressBooks = nil;
     
     addressBooks =  ABAddressBookCreateWithOptions(NULL, NULL);
     //获取通讯录权限
     dispatch_semaphore_t sema = dispatch_semaphore_create(0);
     ABAddressBookRequestAccessWithCompletion(addressBooks, ^(bool granted, CFErrorRef error) {
+        if (!granted) {
+            tip = 1;
+        }
         dispatch_semaphore_signal(sema);
     });
     dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-    
+    if(tip){
+        //用户拒绝访问通讯录
+        UILabel *tipLb = [[UILabel alloc] initWithFrame:CGRectMake(mygap*2, 0, BOUNDS.size.width-4*mygap, 50)];
+        tipLb.center = self.view.center;
+        tipLb.numberOfLines = 0;
+        tipLb.textColor = [UIColor lightGrayColor];
+        tipLb.text = @"请您设置允许APP访问您的通讯录\n设置>隐私>通讯录";
+        tipLb.textAlignment = NSTextAlignmentCenter;
+        [self.view addSubview:tipLb];
+    }
+    else{
     //获取通讯录中的所有人
     CFArrayRef allPeople = ABAddressBookCopyArrayOfAllPeople(addressBooks);
     
     //通讯录中人数
     CFIndex nPeople = ABAddressBookGetPersonCount(addressBooks);
-    
+        [kUserDefault setObject:[NSNumber numberWithInteger:nPeople] forKey:PhoneListCount];
+        [kUserDefault synchronize];
+        ((LVSportViewController*)[((UINavigationController*)[self.navigationController.tabBarController.viewControllers objectAtIndex:0]).viewControllers objectAtIndex:0]).newPeopleCount=0;
     for(NSInteger i = 0; i < nPeople; i++)
     {
         //创建一个addressBook shuxing类
@@ -175,7 +206,7 @@
                         
                     case 0: {// Phone number
                         
-                        addressBook.tel = [[NSMutableArray alloc]initWithObjects:[self mToString: (__bridge NSString*)value],nil];
+                        addressBook.tel = [[NSMutableArray alloc]initWithObjects:[[self mToString: (__bridge NSString*)value] stringByReplacingOccurrencesOfString:@"-" withString:@""],nil];
                         
                         break;
                     }
@@ -200,12 +231,76 @@
         /****************将个人信息添加到数组中********************/
         
         //循环完成后addressBookTemp中包含所有联系人的信息
+        if ([LVTools mToString: [addressBook.tel lastObject]].length==11) {
         [self.contactDic setObject:addressBook forKey:addressBook.localID];
         [_addressBookTemp addObject:addressBook];
         [[SearchCoreManager share] AddContact:addressBook.localID name:addressBook.name phone:addressBook.tel];
+        [_phoneArray addObject:[addressBook.tel lastObject]];
+        }
+        }
         
+    [self requestData];
     }
 }
+- (void)requestData{
+    NSMutableDictionary *paramDic = [LVTools getTokenApp];
+    [paramDic setObject:_phoneArray forKey:@"mobiles"];
+    NSLog(@"paramDic+++%@",paramDic);
+    [self showHudInView:self.view hint:LoadingWord];
+    [DataService requestWeixinAPI:getUsersByMobiles parsms:@{@"param":[LVTools configDicToDES:paramDic]} method:@"post" completion:^(id result) {
+        [self hideHud];
+        NSLog(@"------%@",result);
+        NSArray *array = result[@"data"][@"users"];
+        if ([result[@"status"] boolValue]&&array) {
+            
+            if (array.count>0&&_infoArray==nil) {
+                _infoArray = [NSMutableArray array];
+            
+            for (NSInteger i=0; i<array.count; i++) {
+                FriendListModel *model = [[FriendListModel alloc] init];
+                [model setValuesForKeysWithDictionary:array[i]];
+                //这里用selected属性表示是否好友
+                model.selected = [self didBuddyExist:[LVTools mToString: model.userId]];
+                [_infoArray addObject:model];
+            }
+                //将手机号存在本地用来判断是否有新的联系人
+                
+            }
+            [_tableView reloadData];
+        }
+        else{
+            [self showHint:ErrorWord];
+        }
+    }];
+}
+//判断是否已经发送了申请加好友请求
+- (BOOL)hasSendBuddyRequest:(NSString *)buddyName
+{
+    NSArray *buddyList = [[[EaseMob sharedInstance] chatManager] buddyList];
+    for (EMBuddy *buddy in buddyList) {
+        if ([buddy.username isEqualToString:buddyName] &&
+            buddy.followState == eEMBuddyFollowState_NotFollowed &&
+            buddy.isPendingApproval) {
+            return YES;
+        }
+    }
+    return NO;
+}
+//判断是否在好友列表中
+- (BOOL)didBuddyExist:(NSString *)buddyName
+{
+    NSArray *buddyList = [[[EaseMob sharedInstance] chatManager] buddyList];
+    
+    for (EMBuddy *buddy in buddyList) {
+//        NSLog(@"%@",buddy.username);
+        if ([buddy.username isEqualToString:buddyName] &&
+            buddy.followState != eEMBuddyFollowState_NotFollowed) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
 // 获得区头和单元格的数据
 - (void)getSectionAndCellData {
     
@@ -251,11 +346,13 @@
     _disPlayVC=[[UISearchDisplayController alloc]initWithSearchBar:_searchBar contentsController:self];
     _disPlayVC.delegate=self;
     _disPlayVC.searchResultsTableView.dataSource=self;        // 通讯录表
+    _disPlayVC.searchResultsTableView.delegate = self;
     _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 44, SCREEN_WIDTH, SCREEN_HEIGHT-104) style:UITableViewStylePlain];
     _searchBar.delegate = self;
     _searchBar.placeholder = @"搜索";
     
     _tableView.dataSource =self;
+    _tableView.delegate = self;
     _tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
     if ([self.sortedArrForArrays count]!=0) {
         [self.view addSubview:_tableView];
@@ -366,7 +463,31 @@
     }
     
 }
-
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if ([tableView isEqual:_tableView]) {
+        ChineseString *userInfo = [[self.sortedArrForArrays objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
+        NSLog(@"%@-%@",userInfo.string,userInfo.tel);
+        if (userInfo.tel.length==11&&[LVTools isValidateMobile:userInfo.tel]) {
+            //是手机号
+            FriendListModel *model = [self configByMobile:userInfo.tel];
+            if (model) {
+                WPCFriednMsgVC *msgVc =[[WPCFriednMsgVC alloc] init];
+                msgVc.uid = [LVTools mToString:model.userId];
+                [self.navigationController pushViewController:msgVc animated:YES];
+            }
+            else{
+                [self show:@"TA还未开通校动\n消息将会以免费短信的形式到达对方" icon:@"sadface" view:self.view];
+            }
+        }
+        else{
+            
+        }
+    }
+    else{
+        
+    }
+}
 #pragma mark
 #pragma mark UITableViewDataSource
 #pragma mark
@@ -405,18 +526,17 @@
     else{
         return nil;
     }
-    
 }
-- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView{
-    if ([tableView isEqual:_tableView]) {
-        
-        return self.sectionHeadsKeys;
-    }
-    else{
-        return nil;
-    }
-    
-}
+//- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView{
+//    if ([tableView isEqual:_tableView]) {
+//        
+//        return self.sectionHeadsKeys;
+//    }
+//    else{
+//        return nil;
+//    }
+//    
+//}
 -(UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
     if ([_tableView isEqual:tableView]) {
         
@@ -458,8 +578,24 @@
                 
                 
                 cell.textLabel.text = userNameStr.string;
-                [cell.imageView sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",preUrl,@""]] placeholderImage:[UIImage imageNamed:@"plhor_2"]];
+                [cell.imageView setImage:[UIImage imageNamed:@"plhor_2"]];
                 cell.detailTextLabel.text = userNameStr.tel;
+                cell.addBtn.tag = 1000*indexPath.section+indexPath.row;
+                [cell.addBtn addTarget:self action:@selector(addFriend:) forControlEvents:UIControlEventTouchUpInside];
+                if (_infoArray) {
+                    FriendListModel *model =[self configByMobile:userNameStr.tel];
+                    if (model) {
+                        
+                        [cell.imageView sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",preUrl,model.path]] placeholderImage:[UIImage imageNamed:@"plhor_2"]];
+                        cell.detailTextLabel.text = model.nickName;
+                        if (model.selected) {
+                            cell.addBtn.enabled = NO;
+                        }
+                        else{
+                            cell.addBtn.enabled = YES;
+                        }
+                    }
+                }
             } else {
                 
                 NSLog(@"arr out of range");
@@ -500,12 +636,71 @@
             }
         }
         CoShuXing *contact = [self.contactDic objectForKey:localID];
-        NSLog(@"%@",contact);
+//        NSLog(@"%@",contact);
         cell.textLabel.text = contact.name;
         cell.detailTextLabel.text = matchString;
         //cell.textLabel.text=[_resultArray objectAtIndex:indexPath.row];
     }
     return cell;
+}
+- (void)addFriend:(UIButton*)btn{
+    if (btn.selected==NO) {
+        
+    
+    ChineseString *userInfo = [[self.sortedArrForArrays objectAtIndex:btn.tag/1000] objectAtIndex:btn.tag%1000];
+    NSLog(@"%@-%@",userInfo.string,userInfo.tel);
+    if (userInfo.tel.length==11&&[LVTools isValidateMobile:userInfo.tel]) {
+        //是手机号
+    FriendListModel *model = [self configByMobile:userInfo.tel];
+    if (model) {
+        [self applyAddFriendByMobile:[LVTools mToString:model.userId] withIndex:[NSIndexPath indexPathForRow:btn.tag/1000 inSection:btn.tag%1000]];
+    }
+    else{
+        [self sendMessageByServerWithMobile:userInfo.tel withIndex:[NSIndexPath indexPathForRow:btn.tag/1000 inSection:btn.tag%1000]];
+    }
+    }
+    else{
+        [self show:@"仅支持手机" icon:@"sadface" view:self.view];
+    }
+    }
+}
+/** 注册过的直接添加发送环信好友申请*/
+- (void)applyAddFriendByMobile:(NSString*)mobile withIndex:(NSIndexPath*)index{
+    //环信申请添加好友
+    [self showHudInView:self.view hint:NSLocalizedString(@"friend.sendApply", @"sending application...")];
+    EMError *error;
+    [[EaseMob sharedInstance].chatManager addBuddy:[LVTools mToString: mobile] message:@"申请添加你为好友" error:&error];
+    [self hideHud];
+    if (error) {
+        [self showHint:NSLocalizedString(@"friend.sendApplyFail", @"send application fails, please operate again")];
+    }
+    else{
+//        [self showHint:NSLocalizedString(@"friend.sendApplySuccess", @"send successfully")];
+        [_tableView reloadRowsAtIndexPaths:@[index] withRowAnimation:UITableViewRowAnimationNone];
+    }
+
+}
+/**未注册的直接后台发短信*/
+- (void)sendMessageByServerWithMobile:(NSString*)mobile withIndex:(NSIndexPath*)index{
+    NSMutableDictionary *paramDic = [LVTools getTokenApp];
+    [paramDic setObject:mobile forKey:@"mobile"];
+    NSLog(@"paramDic+++%@",paramDic);
+    [self showHudInView:self.view hint:LoadingWord];
+    [DataService requestWeixinAPI:sendInvitationMessage parsms:@{@"param":[LVTools configDicToDES:paramDic]} method:@"post" completion:^(id result) {
+        [self hideHud];
+        NSLog(@"------%@",result);
+        if ([result[@"status"] boolValue]) {
+         [_tableView reloadRowsAtIndexPaths:@[index] withRowAnimation:UITableViewRowAnimationNone];
+        }
+    }];
+}
+- (FriendListModel*)configByMobile:(NSString*)mobile{
+    for (FriendListModel *model in _infoArray) {
+        if ([mobile isEqualToString:model.mobile]) {
+            return model;
+        }
+    }
+    return nil;
 }
 -(void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
     // 获得所点button的cell
@@ -563,7 +758,10 @@
             chineseString.pinYin = @"";
         }
         CoShuXing *infoBean = [self getInfoByName:chineseString.string];
-        chineseString.tel = [infoBean.tel lastObject];
+        if ([infoBean.tel lastObject]) {
+            chineseString.tel = [infoBean.tel lastObject];
+        }
+        
         [chineseStringsArray addObject:chineseString];
         
     }
@@ -609,7 +807,6 @@
             }
         }
     }
-    
     return arrayForArrays;
     
 }
